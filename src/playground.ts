ed33afeba1cbe8ee1cff9c093ef20f25074ef0d5
Arnau Sanchez <pyarnau@gmail.com>
2025-12-07 20:27:56 +0100

    Clean files

--- src/playground.ts
+++ src/playground.ts
@@ -1,39 +1,29 @@
-import SuperJSON from 'superjson';
-
-type AllExtendT<Args extends any[], T> = {
-    [K in keyof Args]: Args[K] extends T ? true : false;
-};
-
-type T2 = AllExtendT<[string, number, boolean, Date], JsonValue>;
-//type T3 = {[K in T2]: T2[K] extends false ? never : true};
-//   ^?
-
-type JsonValue = string | number | boolean | null | JsonValue[] | { [key: string]: JsonValue };
+import SuperJSON from "superjson";
 
 class Car {
     constructor(
         public make: string,
-        public model: string
+        public model: string,
     ) {}
 }
 
 SuperJSON.registerCustom<Car, { make: string; model: string }>(
     {
-        isApplicable: (value): value is Car => {return value instanceof Car},
-        serialize: car => {return { make: car.make, model: car.model }},
-        deserialize: attrs => {return new Car(attrs.make, attrs.model)}
+        isApplicable: (value): value is Car => value instanceof Car,
+        serialize: car => ({ make: car.make, model: car.model }),
+        deserialize: attrs => new Car(attrs.make, attrs.model),
     },
-    'Car'
+    "Car",
 );
 
-const carStr = SuperJSON.stringify(new Car('Honda', 'Civic'));
-console.log('Serialized:', carStr);
+const carStr = SuperJSON.stringify(new Car("Honda", "Civic"));
+console.log("Stringified:", carStr);
 
 const car = SuperJSON.parse(carStr);
-console.log('Deserialized:', car);
+console.log("Parsed:", car);
 
-const serialized = SuperJSON.serialize(new Car('Ford', 'Focus'));
-console.log('Serialized (object):', serialized);
+const serialized = SuperJSON.serialize(new Car("Ford", "Focus"));
+console.log("Serialized:", serialized);
 
 const deserialized = SuperJSON.deserialize<Car>(serialized);
-console.log('Deserialized (object):', deserialized);
+console.log("Deserialized:", deserialized);
